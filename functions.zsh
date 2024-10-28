d3ca589ecf7b4c110508567e15e3eeba2dd455f4
Kristen Cheung <krcheung9@gmail.com>
2024-10-28 12:25:08 -0400

    add function for git current branch

--- functions.zsh
+++ functions.zsh
@@ -5,3 +5,14 @@ bstack() { git reflog | grep checkout | cut -d " " -f 8 | uniq | head -n ${1} | 
 
 # j -- Autojump
 [[ -s /Users/kristencheung/.autojump/etc/profile.d/autojump.sh ]] && source /Users/kristencheung/.autojump/etc/profile.d/autojump.sh
+
+function git_current_branch() {
+  local ref
+  ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2> /dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return  # no git repo.
+    ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) || return
+  fi
+  echo ${ref#refs/heads/}
+}
