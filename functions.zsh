914f3417b7e33521c83fa8d126a662c7f2c12709
Kristen Cheung <krcheung9@gmail.com>
2024-10-29 23:20:29 -0400

    modify functions

--- functions.zsh
+++ functions.zsh
@@ -1,10 +1,14 @@
-### Functions 
+## Functions 
 
 # print last used branches
 bstack() { git reflog | grep checkout | cut -d " " -f 8 | uniq | head -n ${1} | cat -n; }
 
 # j -- Autojump
 [[ -s /Users/kristencheung/.autojump/etc/profile.d/autojump.sh ]] && source /Users/kristencheung/.autojump/etc/profile.d/autojump.sh
+
+function __git_prompt_git() {
+  GIT_OPTIONAL_LOCKS=0 command git "$@"
+}
 
 function git_current_branch() {
   local ref
