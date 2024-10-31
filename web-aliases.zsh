ceeb87eefd041576017b24fbb22473b4fa497ba4
Kristen Cheung <krcheung9@gmail.com>
2024-10-30 23:48:42 -0400

    modify quotes

--- web-aliases.zsh
+++ web-aliases.zsh
@@ -1,3 +1,7 @@
-alias pi="pnpm i"
-alias prd="pnpm run dev"
-alias prb="pnpm run build"
+alias pi='pnpm i'
+alias prd='pnpm run dev'
+alias prb='pnpm run build'
+
+alias ni='npm i'
+alias nrd='npm run dev'
+alias nrb='npm run build'
